a3d94b3924321940f34d8cdd1effea2480a69e84
chendyreva <nat-217@mail.ru>
2018-07-21 16:35:43 +0500

    Переделанный сервис для чата

---
+++ Server.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.homework6;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+            ServerSocket serv = null;
+            Socket sock = null;
+            String clientName = " ";
+            try {
+                serv = new ServerSocket(1234);
+                System.out.println("Сервер запущен, ожидаем подключения...");
+                sock = serv.accept();
+                System.out.println("Клиент подключился");
+                Scanner sc = new Scanner(sock.getInputStream());
+                PrintWriter pw = new PrintWriter(sock.getOutputStream());
+                while (true) {
+                    String str = sc.nextLine();
+                    if (str.equals("end")) break;
+                    pw.println( clientName + " " + str);
+                    pw.flush();
+                }
+            }
+            catch(IOException e){
+                System.out.println("Ошибка инициализации сервера");
+            } finally{
+                try {
+                    serv.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            //
+        }
+}
